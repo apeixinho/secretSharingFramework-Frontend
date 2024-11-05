b166c86cdeed52efbaec1b5ceffaf5cb6b0da50b
Adolfo Peixinho <a_peixinho@sapo.pt>
2024-11-05 11:57:42 +0000

    Upgrade to Angular 18

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,22 +10,15 @@ import { HeaderComponent } from './header/header.component';
 import { RecoverSecretComponent } from './recover-secret/recover-secret.component';
 import { SplitSecretComponent } from './split-secret/split-secret.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SplitSecretComponent,
-    RecoverSecretComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    AppMaterialModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        SplitSecretComponent,
+        RecoverSecretComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        AppMaterialModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
